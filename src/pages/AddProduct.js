426af5ea916ae4a38c1271bdb82186eb422b3557
Un-Knwn-me <raghul9510@gmail.com>
2023-12-29 21:34:17 +0530

    prod 3.7

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -437,7 +437,7 @@ let removeFormFields = (i) => {
                             autoComplete="care"
                             className="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                             placeholder="Care"
-                            value={style}
+                            value={care}
                             onChange={(e) => setCare(e.target.value)}
                           />
                         </div>
